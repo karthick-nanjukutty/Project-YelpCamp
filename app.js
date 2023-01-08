860b2c7551a964ff19e1924ccf92695d950e3c06
knanjukutty <nanjukutty.karthick@gmail.com>
2023-01-08 07:04:23 -0500

    define schema,model and connect to db

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const engine = require('ejs-mate');
+//const engine = require('ejs-mate');
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 const path = require('path');
@@ -9,4 +9,28 @@ app.set('view engine', 'ejs');
 var methodOverride = require('method-override');
 app.use(methodOverride('_method'));
 const mongoose = require('mongoose');
+const YelpCamp = require('./models/campground')
 
+app.get ('/', (req,res) =>{
+    res.send('Welcome to HomePage')
+})
+
+app.listen(3009,()=>{
+    console.log("Welcome to Yelcamp on port 3009");
+})
+
+
+
+main().catch(err => console.log('OH NO ERROR', err));
+async function main () {
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/campyelp');
+    console.log("Mongo connection Open for YelpCamp New")
+
+    }
+
+    catch (err) {
+        console.log  ("Oh No Error!!! ", err)
+    }
+    
+}
---
+++ models/campground.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const yelpSchema = new Schema ({
+
+    title: {
+        type: String,
+        required: true
+    },
+
+    price: {
+        type: Number,
+        required: true
+    },
+
+    description: {
+        type: String
+    }, 
+
+    location: {
+        type: String
+    }, 
+
+    image: {
+        type: String
+    }
+
+
+}); 
+
+const YelpCamp = mongoose.model('YelpCamp', yelpSchema);
+module.exports = YelpCamp
+
