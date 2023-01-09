b3b8aca3f53f9bc899e0bfa46c135bbb315b9103
knanjukutty <nanjukutty.karthick@gmail.com>
2023-01-09 06:11:54 -0500

    added image and description to seeds and used in show page

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -44,7 +44,8 @@ const seedDb = async () =>{
           title: `${sampleTitle(descriptors)} ${sampleTitle(places)}`,
           location: `${cities[random1000].city},${cities[random1000].state}`,
           image: 'https://source.unsplash.com/collection/3846912',
-          price: priceRandom
+          price: priceRandom,
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
         }).save().then (res =>{
             console.log('Many products Added' , res)
         }). catch (err =>{
