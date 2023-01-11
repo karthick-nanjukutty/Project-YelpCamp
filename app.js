2bb60fbff8554f4a98ccf89b841d5cb3fc2342b9
knanjukutty <nanjukutty.karthick@gmail.com>
2023-01-11 06:25:28 -0500

    add location,image,descrition to joi validation schema

--- app.js
+++ app.js
@@ -56,6 +56,10 @@ app.post ('/campgrounds' , wrapAsync(async(req,res,next) =>{
         campground: Joi.object({
             title: Joi.string().required(),
             price: Joi.number().required().min(0),
+            location : Joi.string().required(),
+            image: Joi.string().required(),
+            description: Joi.string().required()
+
         }).required()
     })
     const {campground} = req.body
