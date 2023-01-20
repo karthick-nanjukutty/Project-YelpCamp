cdf1a47375e227442cd0094aebf7f54261090d9c
knanjukutty <nanjukutty.karthick@gmail.com>
2023-01-20 04:49:34 -0500

    add logout

--- routes/userregistration.js
+++ routes/userregistration.js
@@ -44,4 +44,14 @@ router.post('/login', passport.authenticate('local' , { failureFlash: true, fail
     
     
 })
+
+router.get('/logout' ,(req,res)=>{
+    req.logout((err) =>{
+        if(err) {
+            return next(err)
+        }
+    });
+    req.flash('success' , "Goodbye");
+    res.redirect('/campgrounds')
+})
 module.exports = router;
