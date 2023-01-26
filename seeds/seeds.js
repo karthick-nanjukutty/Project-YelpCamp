a7e1032ce8f38394ac5cb3b0fff8e2e4e20a9503
knanjukutty <nanjukutty.karthick@gmail.com>
2023-01-26 04:30:09 -0500

    add clusermap and reseed db with geometry coordinates from cities

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -47,7 +47,7 @@ const seedDb = async () =>{
           title: `${sampleTitle(descriptors)} ${sampleTitle(places)}`,
           location: `${cities[random1000].city},${cities[random1000].state}`,
           //image: 'https://source.unsplash.com/collection/3846912',
-         geometry: { type: 'Point', coordinates: [ 77.2090057, 28.6138954 ] },
+         geometry: { type: 'Point', coordinates: [ cities[random1000].longitude,cities[random1000].latitude]  },
           price: priceRandom,
           images: [
             {
